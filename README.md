fb9f221a4bc37defe7bc42e3a6cb242c2bb04a2b
BritishPandaChick <sbartley1080@gmail.com>
2022-06-16 01:09:37 -0400

    Update README. Remove old code. Start updating index.html and App.js files

--- README.md
+++ README.md
@@ -8,13 +8,15 @@ freeCodeCamp Frontend Libraries project. Drum Machine.
 * React
 
 ## Instructions 
-Clone this repo or download the zip file. Run ```npm start``` to start the app.
+Clone this repo or download the zip file. Run ```npm start``` to start the app. Click the drum pad elements to start playing sounds.
 
 ## Contributions
 This repository is based on starter code from the create-react-app and Ben Brooke's Drum Machine tutorial. Therefore. I will not be accepting any pull requests.
 
 ## Acknowledgements
+* Dylan Israel 
 * Ben Brooke
-* ReactJS
+* k-dev
+* React
 * Bootstrap
 * freeCodeCamp
--- src/App.js
+++ src/App.js
@@ -1,99 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 
-const sounds = [
-  { id: 'bells', letter: 'Q', src: 'http://dight310.byu.edu/media/audio/FreeLoops.com/2/2/Christmas%20Bells%20Loop-18474-Free-Loops.com.mp3' },
-  { id: 'santa', letter: 'W', src:'http://thecyberbuddy.com/sounds/ho-ho-ho.wav' },
-  { id: 'sleigh bells', letter: 'E', src: 'http://sfxcontent.s3.amazonaws.com/soundfx/SleighBells3.mp3' },
-  { id: 'champagne', letter: 'A', src: 'http://sfxcontent.s3.amazonaws.com/soundfx/ChampagnePopPour.mp3' },
-  { id: 'turkey', letter: 'S', src: 'http://courses.cs.vt.edu/~cs1004/turkeyGobble.mp3' },
-  { id: 'party horn', letter: 'D', src: 'http://dight310.byu.edu/media/audio/FreeLoops.com/5/5/Party%20Horn.wav-22854-Free-Loops.com.mp3' },
-  { id: 'waterfall', letter: 'Z', src: 'http://csfiles.maniapc.org/cs/sound/ambience/waterfall1.wav' },
-  { id: 'rain', letter: 'X', src: 'http://msh38.ivyro.net/bookphotoshopdata2/rain03.wav' },
-  { id: 'stream', letter: 'C', src:'http://koo.corpus.cam.ac.uk/naturesound/waves/riviere3.wav' },
-];
+function App() {
+  //Array of keycodes and sounds
+  const drumPads = [
+    {
+      keyCode: 81,
+      text: 'Q',
+      src: 'http://dight310.byu.edu/media/audio/FreeLoops.com/2/2/Christmas%20Bells%20Loop-18474-Free-Loops.com.mp3',
+    },
+    {
+      keyCode: 87,
+      text: "W",
+      src: "http://thecyberbuddy.com/sounds/ho-ho-ho.wav",
+    },
+    {
+      keyCode: 69,
+      text: "E",
+      src: "http://sfxcontent.s3.amazonaws.com/soundfx/SleighBells3.mp3",
+    },
+    {
+      keyCode: 65,
+      text: "A",
+      src: "http://sfxcontent.s3.amazonaws.com/soundfx/ChampagnePopPour.mp3",
+    },
+    {
+      keyCode: 83,
+      text: "S",
+      src: "http://courses.cs.vt.edu/~cs1004/turkeyGobble.mp3",
+    },
+    {
+      keyCode: 68,
+      text: "D",
+      src: "http://dight310.byu.edu/media/audio/FreeLoops.com/5/5/Party%20Horn.wav-22854-Free-Loops.com.mp3",
+    },
+    {
+      keyCode: 90,
+      text: "Z",
+      src: "http://csfiles.maniapc.org/cs/sound/ambience/waterfall1.wav",
+    },
+    {
+      keyCode: 88,
+      text: "X",
+      src: "http://msh38.ivyro.net/bookphotoshopdata2/rain03.wav",
+    },
+    {
+      keyCode: 87,
+      text: "C",
+      src: "http://koo.corpus.cam.ac.uk/naturesound/waves/riviere3.wav",
+    },
+  ];
 
-class DrumPad extends React.Component {
-
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyDown);
-  }
-
-  /* Get the key code for event listener so sound wil play */
-  handleKeyDown = e => {
-    if(e.keyCode === this.props.letter.charCodeAt()) {
-      this.audio.play()
-      this.audio.currentTime = 0;
-      this.props.handleDisplay(this.props.id);
-    }
-  }
-
-  handleClick = () => {
-    this.audio.play()
-    this.audio.currentTime = 0; //allows you to play two sounds close to each other. Lets you activate sounds quicker
-    this.props.handleDisplay(this.props.id); //pass description want to display
-  }
-
-  render() {
-    return (
-      <div
-        className="drum-pad"
-        id={this.props.id}
-        onClick={this.handleClick}
-       >
-        <p>{this.props.letter}</p>
-        <audio
-          src={this.props.src}
-          id={this.props.letter}
-          className="clip"
-          ref={ref => this.audio = ref}>
-        </audio>
-      </div>
-    )
-  }
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      display: ""
-    }
-  }
-
-  handleDisplay = display => this.setState({ display }); //setting display property to what it takes in
-
-  render() {
-    return (
+  return (
+    <div className="App">
       <div id="drum-machine">
-        <div class="container">
-          <div class="row">
-            <div id="display">{this.state.display}</div>
-              <div id="drum-pads">
-                {sounds.map(s => (
-                <DrumPad
-                  id={s.id}
-                  letter={s.letter}
-                  src={s.src}
-                  handleDisplay={this.handleDisplay}
-                  />
-                ))}
-              </div>
-          </div>
-        </div>
+        <div id="display"></div>
+        //Setup the drum pads
+        {drumPads.map((drumPad) => <div className="drum-pad" id={drumPad.text}>{drumPad.text}</div>)}
       </div>
-    )
-  }
+    </div>
+  );
 }
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
 
 export default App;
